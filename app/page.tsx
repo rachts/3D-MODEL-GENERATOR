248a3e61762c8929ce3006bd663f55459bafa013
Rachit <rachit.17.01.2004@gmail.com>
2025-12-31 01:28:12 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Rachit3DGenerator from "@/components/rachit-3d-generator"
+
+export default function Home() {
+  return (
+    <main className="h-[100dvh] w-screen overflow-hidden bg-black bg-radial-gradient">
+      <Rachit3DGenerator />
+    </main>
+  )
+}
